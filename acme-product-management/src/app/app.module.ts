7bb1bee06bd0393b50293ffd9affc5ca16a74893
nsiddiqui25 <n.siddiqui25@gmail.com>
2019-06-13 14:03:55 -0500

    bootstrapping AppComponent

--- acme-product-management/src/app/app.module.ts
+++ acme-product-management/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// NgModule decorator, defining this class as an Angular module
 @NgModule({
+  //declares the AppComponent so that Angular can locate its selector
   declarations: [
     AppComponent
   ],
+  //imports array includes BrowserModule so that app runs correctly in the browser
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
+  //bootstrap array lists our AppComponent as the starting component for our app
   bootstrap: [AppComponent]
 })
+
+//Class
 export class AppModule { }
