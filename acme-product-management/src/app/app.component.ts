ef8a5652b2d9b7f2627b96631c28947d6176dbac
nsiddiqui25 <n.siddiqui25@gmail.com>
2019-06-13 12:20:34 -0500

    creating our app component

--- acme-product-management/src/app/app.component.ts
+++ acme-product-management/src/app/app.component.ts
@@ -1,3 +1,4 @@
+/* 
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'acme-product-management';
+}
+*/
+
+// 3) Importing
+  // IntelliSense automatically filled this in, otherwise the component decorator would flag an error which is that it can't find the name
+import { Component } from '@angular/core';
+
+
+// 2) Component
+@Component({
+  selector: 'pm-root',
+  template: `
+  <div>
+    <h1>{{pageTitle}}</h1>
+    <p>My First Component</p>
+  </div>
+  `
+})
+
+
+// 1) Building the class
+export class AppComponent {
+  pageTitle: string = 'Acme Product Management';
 }
